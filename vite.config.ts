16a030f401a89075f06e55146f2de5bc61ed15da
artursopelnik <artur.sopelnik93@gmail.com>
2024-09-04 17:53:36 +0200

    Improve actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/microfrontend-with-systemjs/' // GitHub Pages Support https://github.com/sitek94/vite-deploy-demo
 })
